db0e4355628abc4f38aa6aed82b462a9ff9625fb
Betsa Meneses <betsadmd@gmail.com>
2023-02-15 12:12:16 +0100

    food section structure

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 $(document).ready(function( $ ) {
     $( ".home" ).first().addClass( "open" );
+    $('#food-section').css("display","none");
 });
 
 // menu vertical
@@ -10,13 +11,13 @@ tl.to(".item-yellow", {y: '0', duration:0.2});
 tl.to("h1", {opacity:1, duration: 1.3, ease:Power1.easeOut}, ">");
 
 
-
 // menu open&close
 $(".panel").click(function() {
     $('.panel').removeClass("open");
     $(this).toggleClass("open");
     $('.panel > p, .panel > span').css("opacity", 1);
-    $(this).children("p, span").css("opacity", 0);   
+    $(this).children("p, span").css("opacity", 0);
+      
 });
 
 $(".panel.open").click(function() { $(this).removeClass("open"); 
@@ -24,17 +25,36 @@ $(".panel.open").click(function() { $(this).removeClass("open");
 
 // Logo animation
 $(".item-yellow, .item-blue, .item-red").click(function() {
+    $('.home-content').css("display","none"); 
     gsap.to(".logo", {height: '40px', width: '40px', left: '10px', duration: 0.3});
 });
 
 $(".home").click(function() { 
     gsap.to(".logo", {height: '70px', width: '70px', left: '60px', duration: 0.3});
-    gsap.to( 'main', {'margin-left': '0', duration: 0.3});
+    gsap.to( '.home-content', {'margin-left': '0', duration: 0.3});
+    $('#food-section').css("display","none"); 
 });
 
-$(".item-yellow").click(()=> $('main').css("marginLeft", 60));
-$(".item-blue").click(()=> $('main').css("marginLeft", 120));
-$(".item-red").click(()=> $('main').css("marginLeft", 180));
+$(".home").click(()=> {
+    $('footer, main').css("marginLeft", 0);
+    $('.home-content').css("display","block");
+    $('#food-section').css("display","none"); 
+});
+
+$(".item-yellow").click(()=>{ 
+    $('footer, main').css("marginLeft", 60)
+    $('#food-section').css("display","block");
+});
+
+$(".item-blue").click(()=> {
+    $('footer').css("marginLeft", 120);
+    $('#food-section').css("display","none");
+});
+
+
+$(".item-red").click(()=> $('footer').css("marginLeft", 180));
+
+
 
 // Fade photos
 gsap.to("#section-01 > img", {
